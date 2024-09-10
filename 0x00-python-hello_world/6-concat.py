fd61d89ce08773289a7b377ec2ea15ea4e79f003
DzeBakuEso <dzeblebaku@gmail.com>
2024-09-10 03:22:56 -0700

    6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3 
-str1 = "Holberton"
-str2 = "School"
-str1 = str1+ " " +str2
-print(f"Welcome to {str1}!"
-        )
+#!/usr/bin/python3
+str1 = "Holberton"  # Define the first part of the string
+str2 = "School"  # Define the second part of the string
+str1 = str1 + " " + str2  # Concatenate str1 and str2 with a space in between
+print(f"Welcome to {str1}!")  # "Welcome to Holberton School!"
+
