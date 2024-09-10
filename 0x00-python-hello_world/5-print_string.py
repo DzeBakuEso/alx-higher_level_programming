fc7ecae2a593dbcc72a65839d22da092e7413cf7
DzeBakuEso <dzeblebaku@gmail.com>
2024-09-10 03:07:07 -0700

    5-print_string.py

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print(str * 3)  # This prints the string 3 times in a row without spaces
+print(str[:9])  # This prints the first 9 characters of the string
+
