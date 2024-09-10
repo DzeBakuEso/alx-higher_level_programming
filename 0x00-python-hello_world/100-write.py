f2a45d6fecd13ceaa8a4eaa59206393ddf96ce84
DzeBakuEso <dzeblebaku@gmail.com>
2024-09-10 10:51:34 -0700

    100-write.py

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys  # Import the sys module to work with system-level functions
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")  # Write message to stderr
+sys.exit(1)  # Exit with status code 1
+
