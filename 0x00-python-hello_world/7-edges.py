c3a301f8084fd1c43ca1a2b98fe302c72a968728
DzeBakuEso <dzeblebaku@gmail.com>
2024-09-10 03:37:39 -0700

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"  # Define the string word
+word_first_3 = word[:3]  # Extract the first 3 letters
+word_last_2 = word[-2:]  # Extract the last 2 letters
+middle_word = word[1:-1]  # Extract the middle part, removing the first and last letters
+print(f"First 3 letters: {word_first_3}")  # Output the first 3 letters
+print(f"Last 2 letters: {word_last_2}")  # Output the last 2 letters
+print(f"Middle word: {middle_word}")  # Output the middle part
+
