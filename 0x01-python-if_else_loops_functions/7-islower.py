3a103ac36c4616d9b5fa3ab1d9dda0e3e85724cd
DzeBakuEso <dzeblebaku@gmail.com>
2024-09-10 22:28:13 -0700

    7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3  # Shebang to specify Python interpreter
+
+def islower(c):
+    """Check if the character c is a lowercase letter."""
+    return ord('a') <= ord(c) <= ord('z')
+
