3e5de2b2126d85dad7dfd760d56fbff4a8a1fbd0
DzeBakuEso <dzeblebaku@gmail.com>
2024-09-19 04:22:38 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Use a list comprehension to create a new matrix with squared values
-    return[[x ** 2 for x in row] for row in matrix]
+    """
+    This function squares all integers in the input 2D matrix.
+    A new matrix is returned without modifying the input matrix.
+    """
+    # Use list comprehension to create a new matrix with squared values
+    return [[x ** 2 for x in row] for row in matrix]
