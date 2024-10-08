c694662c935eff298b6c141d6ce325a432abb8ff
DzeBakuEso <dzeblebaku@gmail.com>
2024-10-07 19:01:17 -0700

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """This defines a rectangle by its width and height"""  
+    """This defines a rectangle by its width and height"""
     def __init__(self, width=0, height=0):
         """This initializes the rectangle with optional width and height."""
         self.width = width
