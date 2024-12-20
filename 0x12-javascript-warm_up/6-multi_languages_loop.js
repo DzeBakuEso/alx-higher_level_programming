538d49e34fbd7347e731a05b169c0add53081278
DzeBakuEso <dzeblebaku@gmail.com>
2024-12-19 17:19:24 -0800

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * Prints the elements of an array using a loop.
+ * The array contains the phrases: "C is fun", "Python is cool", and "JavaScript is amazing".
+ */
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+/**
+ * Using a loop to print each element of the array.
+ */
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
