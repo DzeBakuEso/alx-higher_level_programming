7fb22d5d2301ef9d3f436bff4d4b0f83a4100a40
DzeBakuEso <dzeblebaku@gmail.com>
2024-12-19 16:32:45 -0800

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* Exclude the first two elements (script name and node) */
+const args = process.argv.length - 2;
+
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments');
+}
