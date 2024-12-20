45ad5cb4d427afb257646bcef4282bf9f162f7e8
DzeBakuEso <dzeblebaku@gmail.com>
2024-12-19 18:00:10 -0800

    13-main.js 13-add.js

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * Function that adds two integers and returns the result
+ * The function is visible from outside the file
+ * @param {number} a - First integer
+ * @param {number} b - Second integer
+ * @returns {number} The sum of a and b
+ */
+exports.add = function (a, b) {
+  return a + b;
+};
---
+++ 0x12-javascript-warm_up/13-main.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * Import the `add` function from 13-add.js
+ */
+const add = require('./13-add').add;
+
+/**
+ * Call the add function with 3 and 5 as arguments and print the result
+ */
+console.log(add(3, 5));
