278e85a2ba0713941b157bc8c68668b69aca366d
DzeBakuEso <dzeblebaku@gmail.com>
2024-12-19 16:57:03 -0800

    add comments

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-/* this 3 simple print statement*/
+/* this 3 simple print statement */
 console.log('C is fun');
 console.log('Python os cool');
 console.log('JavaScript is amazing');
