2d78f7e21169fdedb90cbe72a934a3969acd7473
DzeBakuEso <dzeblebaku@gmail.com>
2024-12-28 05:39:29 -0800

    model_state.py, 6-model_state.py, 6-model_state.sql

---
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0e_6_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
+
+-- Use the database
+USE hbtn_0e_6_usa;
+
+-- Show the structure of the 'states' table
+SHOW CREATE TABLE states;
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines the State class and Base instance for SQLAlchemy.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create the Base instance
+Base = declarative_base()
+
+class State(Base):
+    """
+    Represents a state for a MySQL database.
+
+    Attributes:
+        id (int): The state's id, a primary key that is auto-generated.
+        name (str): The state's name, a string of up to 128 characters.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
