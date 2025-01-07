fb243d0c5733b4f6977a518378cfe8558e8dbc4e
DzeBakuEso <dzeblebaku@gmail.com>
2025-01-06 17:01:16 -0800

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,11 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Create an engine to connect to the MySQL server
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}', pool_pre_ping=True)
-    
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}', 
+        pool_pre_ping=True
+    )
+
     # Create all tables in the database (if not already created)
     Base.metadata.create_all(engine)
 
