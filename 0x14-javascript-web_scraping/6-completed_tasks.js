e09998da4dd088d76d67daa36e4e799d9288f006
DzebakuEso <dzeblebaku@gmail.com>
2025-02-11 14:04:10 -0800

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasks = {};
+
+    tasks.forEach((task) => {
+      if (task.completed) {
+        completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
+      }
+    });
+
+    console.log(completedTasks);
+  }
+});
