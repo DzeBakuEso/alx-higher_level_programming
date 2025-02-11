25bb36f98839900ad72be796a023fa302e4ddac8
DzebakuEso <dzeblebaku@gmail.com>
2025-02-11 14:06:16 -0800

    100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error(charError);
+        } else {
+          console.log(JSON.parse(charBody).name);
+        }
+      });
+    });
+  }
+});
