80a5934ea510fd20170a44ecb895d012b64fa91f
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-19 23:07:51 +0000

    101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,26 @@
+i$(document).ready(function () {
+  $("#add_item").click(function () {
+    $(".my_list").append("<li>Item</li>");
+  });
+
+  $("#remove_item").click(function () {
+    $(".my_list li:last-child").remove();
+  });
+
+  $("#clear_list").click(function () {
+    $(".my_list").empty();
+  });
+});
+i$(document).ready(function () {
+  $("#add_item").click(function () {
+    $(".my_list").append("<li>Item</li>");
+  });
+
+  $("#remove_item").click(function () {
+    $(".my_list li:last-child").remove();
+  });
+
+  $("#clear_list").click(function () {
+    $(".my_list").empty();
+  });
+});
